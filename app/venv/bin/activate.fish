30778851b13ac738050ef87d3f82c24915680dc6
gkatsikis <georgioscabro@gmail.com>
2026-02-09 10:48:27 -0500

    add requirements

--- app/venv/bin/activate.fish
+++ app/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /Users/gkats/Projects/HL7-Converter/backend/venv
+set -gx VIRTUAL_ENV /Users/gkats/Projects/HL7-Converter/app/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
